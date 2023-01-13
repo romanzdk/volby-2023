a64c3dacd728a8fb228d3b503cdb3297444b07df
Roman Zydyk <romanzdk@gmail.com>
2023-01-13 07:21:27 +0100

    Ignore pycache

---
+++ db.sql
@@ -0,0 +1,47 @@
+CREATE TABLE overall_over_time (
+	"last_update" TIMESTAMP,
+	"Babiš" NUMERIC,
+	"Nerudová" NUMERIC,
+	"Pavel" NUMERIC,
+	"Bašta" NUMERIC,
+	"Diviš" NUMERIC,
+	"Fischer" NUMERIC,
+	"Hilšer" NUMERIC,
+	"Zima" NUMERIC,
+);
+INSERT INTO public.overall_over_time
+VALUES (
+		'2023-1-13 14:00',
+		0.25,
+		0.33,
+		0.42,
+		0.1,
+		0.1,
+		0.1,
+		0.1,
+		0.1
+	);
+INSERT INTO public.overall_over_time
+VALUES (
+		'2023-1-13 15:00',
+		0.55,
+		0.25,
+		0.20,
+		0.1,
+		0.1,
+		0.1,
+		0.1,
+		0.1
+	);
+INSERT INTO public.overall_over_time
+VALUES (
+		'2023-1-13 16:00',
+		0.80,
+		0.10,
+		0.10,
+		0.1,
+		0.1,
+		0.1,
+		0.1,
+		0.1
+	);
