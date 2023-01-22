9fc692b85b4e2eccefcf38e608bc49c95801d8b5
Roman Zydyk <romanzdk@gmail.com>
2023-01-22 16:59:39 +0100

    Solve issues with default theme

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-mkdir -p ~/.streamlit/
 echo """
 [server]
 headless = true
 port = $PORT
-enableCORS = false""" > ~/.streamlit/config.toml
-streamlit run volby_2023/application.py --theme.base "light"
+enableCORS = false""" >> streamlit/config.toml
+streamlit run volby_2023/application.py 
